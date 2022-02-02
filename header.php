d414cfcb5bf4bd7552555e3c6674ad79418a505f
IH Shadin <shadinfr@gmail.com>
2022-02-02 16:35:33 +0600

    Preloader Uncomment

--- header.php
+++ header.php
@@ -16,16 +16,16 @@
    <body <?php body_class();?>>
        
     <!-- Preloader Start -->
-    <!-- <div id="preloader-active">
+    <div id="preloader-active">
         <div class="preloader d-flex align-items-center justify-content-center">
             <div class="preloader-inner position-relative">
                 <div class="preloader-circle"></div>
                 <div class="preloader-img pere-text">
-                    <img src="<?php// echo get_template_directory_uri();?>/assets/img/logo/logo.png" alt="">
+                    <img src="<?php echo get_template_directory_uri();?>/assets/img/logo/logo.png" alt="">
                 </div>
             </div>
         </div>
-    </div> -->
+    </div>
     <!-- Preloader Start -->
 
     <header>
